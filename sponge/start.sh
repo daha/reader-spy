8a923d8cb1cf3e6432b9747aa9716c13b37232f9
David Haglund <github2@davidhaglund.se>
2012-02-04 14:59:42 +0100

    Compress the data files before starting and remove empty files

--- sponge/start.sh
+++ sponge/start.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 script_dirname=`dirname $0`
 script_dir=`(cd $script_dirname; pwd)`
+host=`hostname | cut -d'.' -f1`
 
 cd $script_dir
+
+find data -name $host-\* -size 0 -delete
+gzip -q data/$host-*
 
 erl -sname sponge -pa ebin -pa deps/*/ebin \
     -boot start_sasl -s sponge
